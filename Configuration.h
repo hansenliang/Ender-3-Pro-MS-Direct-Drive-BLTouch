fc4f5bf494ac552e6ea79b89c50d57ed7c9417bb
hanshawx <hanshawx@gmail.com>
2020-06-28 14:23:13 -0400

    Update Configuration.h
    Fixed Y movement range. (Y_min_pos)

--- Configuration.h
+++ Configuration.h
@@ -1092,10 +1092,10 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 12 // Micro Swiss Direct Drive + Hot End offset
+#define Y_MIN_POS -13 // Micro Swiss Direct Drive + Hot End offset
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE + 12 // Micro Swiss Direct Drive + Hot End offset
+#define Y_MAX_POS Y_BED_SIZE // Micro Swiss Direct Drive + Hot End offset
 #define Z_MAX_POS 250
 
 /**
